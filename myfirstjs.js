4ea0bb1215717fdc89503896eede0b6a4142ba7b
Bin-0y <91381031+Bin-0y@users.noreply.github.com>
2022-07-29 14:58:06 +0530

    nn

--- myfirstjs.js
+++ myfirstjs.js
@@ -1,29 +1,27 @@
-//1.
-var http = require('http');
-var fs = require('fs');
-//2.
-var server = http.createServer(function (req, resp) {
-    //3.
-    if (req.url === "/create") {
-        fs.readFile("usernew_cezen/index.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-             
-            resp.end();
-        });
-    } else {
-        //4.
-        resp.writeHead(200, { 'Content-Type': 'text/html' });
-        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
-        resp.end();
+
+import { default as mongodb } from 'mongodb';
+let MongoClient = mongodb.MongoClient;
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb+srv://980binoy:980binoy@cluster0.7rwjshh.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        debugger;
+        const database = client.db("insertDB");
+        const haiku = database.collection("haiku");
+        // create a document to insert
+        const doc = {
+            title: "Record of a Shriveled Datum",
+            content: "No bytes, no problem. Just insert a document, in MongoDB",
+        }
+        const result = await haiku.insertOne(doc);
+
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        await client.close();
     }
-});
-//5.
-server.listen(5050);
- 
-console.log('Server Started listening on 5050');
+}
+run().catch(console.dir);
